ce10214fce9e7b3fc46dfb34d19acbb1b9b5f99f
Lucas Kushner <lphk92@gmail.com>
2015-07-10 16:08:55 -0500

    Functional prototype for ascii languages

--- index.py
+++ index.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, request, url_for
 
+from getwords import get_common_words
+
 app = Flask(__name__)
 
-def get_word_data(url):
-    return {"a": 100, "the": 125}
+#def get_word_data(url):
+    #return {"a": 100, "the": 125}
 
 @app.route("/")
 def index():
+    print "Index time!"
     return render_template("index.html")
 
 @app.route("/form_submit", methods=["POST"])
 def form_submit():
     url = request.form["url"]
-    data = sorted(get_word_data(url))
+    data = get_common_words(url)
+    data = {str(k): data[k] for k in data}
+    print "Got Data!"
     print data
-    return "Success!"
     return render_template("index.html",
+                           url=url,
                            labels=data.keys(),
                            values=data.values())
+
+if __name__ == "__main__":
+   app.run()
