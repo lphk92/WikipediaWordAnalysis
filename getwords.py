daac6fdb4ae76e9cc5a19e8d60c8798c13553b82
Lucas Kushner <lphk92@gmail.com>
2015-07-11 16:53:39 -0500

    Better filtering to only look at words

--- getwords.py
+++ getwords.py
@@ -9,8 +9,13 @@ def get_common_words(url,N=100) :
     resp = urllib2.urlopen(url)
     html = resp.read()
     raw = BeautifulSoup(html).get_text() #is unicode
-    raw = ''.join([c for c in raw if unicodedata.category(c) != 'Po'])
 
-    words = raw.split(" ")
+    # only use characters in the "Letter" and "Spacing" categories
+    valid_prefixes = ["L", "Z"]
+    raw = ''.join([c for c in raw
+                   if unicodedata.category(c)[0] in valid_prefixes])
+
+
+    words = [w for w in raw.split(" ") if len(w) > 0]
     freqs = Counter(words)
     return dict(freqs.most_common(N))
